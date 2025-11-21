e8439837c9104f711757a66e7be32f68aaca88c6
udud0099 <udud0099@gmail.com>
2025-11-21 20:14:26 +0545

    mini projects start

---
+++ projects/miniproject1/bmicalculator - Copy - Copy/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <h1 className='text-8xl text-green-700'>Let's Ready</h1>
+    </>
+  )
+}
+
+export default App
