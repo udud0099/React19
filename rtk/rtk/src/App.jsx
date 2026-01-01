08d999748de80bad1a5162b11f6f651705b79283
udud0099 <udud0099@gmail.com>
2026-01-01 17:20:50 +0545

    search slice is ready

--- rtk/rtk/src/App.jsx
+++ rtk/rtk/src/App.jsx
@@ -1,46 +1,11 @@
-import React from "react";
-import { fetchGIFs, fetchPhotos, fetchVideos } from "./api/mediaApi";
+ 
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
-  const [photos, setPhotos] = React.useState([]);
-
-  const addphotos = async (r) => {
-    setPhotos(await r);
-  };
-  // console.log(photos, "photos --t");
-
+ 
   return (
     <div>
-      App App
-      <br />
-      <br />
-      <br />
-      <button onClick={() => addphotos(fetchPhotos("husky"))}>p click</button>
-      <br />
-      <button onClick={() => addphotos(fetchVideos("husky"))}>v click</button>
-      <br />
-      <button onClick={() => addphotos(fetchGIFs("husky"))}>gif click</button>
-      <br />
-      {/* <div>
-        {photos &&
-          photos.map((photo) => (
-            <img
-              key={photo.id}
-              src={photo.urls.full}
-              alt={photo.alt_description}
-            />
-          ))}
-      </div> */}
-      <div>
-        {photos &&
-          photos.map((photo) => (
-            <img
-              key={photo.id}
-              src={photo.images.fixed_height.url}
-              alt={photo.title}
-            />
-          ))}
-      </div>
+      <SearchBar />
     </div>
   );
 };
