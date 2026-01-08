d5d77285f4fc9dc03ac3c5c0998eb5ade9720da1
udud0099 <udud0099@gmail.com>
2026-01-08 10:06:01 +0545

    weather app - api call and console print and show frontend problem

--- miniproject1/WeatherApp/src/App.jsx
+++ miniproject1/WeatherApp/src/App.jsx
@@ -1,13 +1,12 @@
- 
+import MyFetch from "./components/MyFetch";
 
 function App() {
-  
-
   return (
     <>
-       <h1 className="text-4xl text-red-900">hello</h1>
+      <h1 className="text-4xl text-red-900">hello</h1>
+      <MyFetch />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ miniproject1/WeatherApp/src/components/MyFetch.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+const MyFetch = () => {
+  const [repos, setRepos] = useState([]);
+  const [search, setSearch] = useState("Kathmandu");
+  const [sv, setSv] = useState("");
+
+  const searchFun = () => {
+    setSv(search);
+  };
+
+  useEffect(() => {
+    // fetch call used to be here
+    const data = fetch(
+      //   "https://api.openweathermap.org/geo/1.0/direct?q=Kathmandu&appid=65a843b0e9abbefbcb58359a2af2db8f"
+      ` https://api.openweathermap.org/data/2.5/weather?q=${
+        sv || "Kathmandu"
+      }&units=metric&appid=65a843b0e9abbefbcb58359a2af2db8f`
+    )
+      .then((response) => response.json())
+      .then((data) => setRepos(data));
+  }, [sv]);
+//   console.log(repos.weather[0].main);
+
+  return (
+    <div>
+      MyFetch MyFetch
+      <br />
+      <br />
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="p-4 border border-amber-500 rounded-2xl"
+      />
+      <br />
+      <button onClick={searchFun}>Search</button>
+      <br />
+      <h1>Today Weather is</h1>
+      <h1>City : {repos.name}</h1>
+      {/* <h1>City : {repos.weather[0].description}</h1> */}
+      <h1>weather : {repos.weather[0].main}</h1>
+      <h1>weather : {repos.weather[0].description}</h1>
+    </div>
+  );
+};
+
+export default MyFetch;
